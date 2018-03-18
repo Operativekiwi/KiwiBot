8187a1ffa48bb45c096b53e71e0a82e1933cdb5e
Operative Kiwi <email@operative.kiwi>
2018-03-18 16:31:49 +1100

    Update warnreset.js

--- commands/warnreset.js
+++ commands/warnreset.js
@@ -6,7 +6,6 @@ let warns = JSON.parse(fs.readFileSync("./warnings.json", "utf8"));
 
 module.exports.run = async (bot, message, args) => {
 
-    //+warn @user <reason>
     if(!message.member.hasPermission("MANAGE_MEMBERS")) return message.reply("Insufficient permissions.")
     let wUser = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0])
     if(!wUser) return message.reply("Couldn't find that user.");
