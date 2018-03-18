4f063c4b1ddbfa4fdda93d9d1bcb63efdc3478a1
Operative Kiwi <email@operative.kiwi>
2018-03-18 16:32:21 +1100

    Update addrole.js

--- commands/addrole.js
+++ commands/addrole.js
@@ -1,8 +1,9 @@
+//+addrole @member role 
 const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
 
-    //+addrole @member role 
+
 if(!message.member.hasPermission("MANAGE_MEMBERS")) return message.reply("Sorry, you don't have permission.");
 let rMember = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]);
 if(!rMember) return message.reply("Couldn't find that user.");
