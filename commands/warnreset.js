72bc1a9d02e9e22a5cb6163bca65ae60eaa800cf
Operative Kiwi <email@operative.kiwi>
2018-03-18 17:35:01 +1100

    Update warnreset.js

--- commands/warnreset.js
+++ commands/warnreset.js
@@ -6,8 +6,8 @@ let warns = JSON.parse(fs.readFileSync("./warnings.json", "utf8"));
 
 module.exports.run = async (bot, message, args) => {
 
-    let wUser = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0])
-    if(!message.member.hasPermission("MANAGE_MEMBERS")) return message.reply("Insufficient permissions.")
+    let wUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+    if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send(`You don't have permission ${message.author}`);
     if(!wUser) return message.reply("Couldn't find that user.");
     if(wUser.hasPermission("MANAGE_MESSAGES")) return message.reply("You can't warn this user.");
     let reason = args.join(" ").slice(22);
