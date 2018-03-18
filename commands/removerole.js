6e8d9576fb7419fb059cf1c3ceba6550c492010d
Operative Kiwi <email@operative.kiwi>
2018-03-18 21:42:57 +1100

    Update removerole.js

--- commands/removerole.js
+++ commands/removerole.js
@@ -11,14 +11,16 @@ if(!role) return message.reply("Specify a role!");
 let gRole = message.guild.roles.find(`name`, role);
 if(!gRole) return message.reply("Couldn't find that role.");
 
-if(rMember.roles.has(gRole.id)) return message.reply("They already have the role.");
-await(rMember.removeRole(gRole.id));
+//if(rMember.roles.has(gRole.id)) return message.reply("They already have the role.");
+//await
+(rMember.removeRole(gRole.id));
 
 try{
     await rMember.send(`You have been removed the role ${gRole.name}`)
 }catch(e){
     message.channel.send(`<@${rMember.id}>, has had the role ${gRole.name} removed. We tried to DM them, but their DMs are locked.`);
     }
+message.delete().catch(O_o=>{});
 }
 
 module.exports.help = {
