2f64b27b3d68aaf40402a571cd00d4583bf66383
Operative Kiwi <email@operative.kiwi>
2018-03-18 17:31:36 +1100

    Update warnreset.js

--- commands/warnreset.js
+++ commands/warnreset.js
@@ -1,4 +1,4 @@
-//+warnreset @user
+//+warn @user <reason>
 const Discord = require("discord.js");
 const fs = require("fs");
 const ms = require("ms");
@@ -6,8 +6,8 @@ let warns = JSON.parse(fs.readFileSync("./warnings.json", "utf8"));
 
 module.exports.run = async (bot, message, args) => {
 
-    if(!message.member.hasPermission("MANAGE_MEMBERS")) return message.reply("Insufficient permissions.")
     let wUser = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0])
+    if(!message.member.hasPermission("MANAGE_MEMBERS")) return message.reply("Insufficient permissions.")
     if(!wUser) return message.reply("Couldn't find that user.");
     if(wUser.hasPermission("MANAGE_MESSAGES")) return message.reply("You can't warn this user.");
     let reason = args.join(" ").slice(22);
